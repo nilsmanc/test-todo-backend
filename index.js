5c77be80f6efb25574b43068f961a429ba8344a2
nilsmanc <etereaiberia@gmail.com>
2022-11-19 13:47:54 +0300

    deploy

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
   })
 })
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err)
   }
